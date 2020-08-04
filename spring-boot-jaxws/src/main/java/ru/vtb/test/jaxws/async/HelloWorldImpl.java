62d166c68c767be953af05aefc9ea51f87dc57b5
lyapinv <dfvGbh123>
2020-08-04 11:00:53 +0300

    Коммит не рабочий - нужно разобраться с запуском WS в docker

---
+++ spring-boot-jaxws/src/main/java/ru/vtb/test/jaxws/async/HelloWorldImpl.java
@@ -0,0 +1,24 @@
+package ru.vtb.test.jaxws.async;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.jws.WebService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+//Service Implementation
+@Slf4j
+@WebService(endpointInterface = "ru.vtb.test.jaxws.async.HelloWorld")
+public class HelloWorldImpl implements HelloWorld {
+
+    private final AtomicInteger counter = new AtomicInteger();
+
+    @Override
+    public String getHelloWorldAsString(String name) throws InterruptedException {
+        int executionCount = counter.incrementAndGet();
+        log.info("Current executionCount: {}", executionCount);
+        TimeUnit.SECONDS.sleep(3);
+        return "Hello World JAX-WS " + name;
+    }
+
+}
---
+++ spring-boot-jaxws/src/main/java/ru/vtb/test/jaxws/async/HelloWorldPublisher.java
@@ -0,0 +1,10 @@
+package ru.vtb.test.jaxws.async;
+
+import javax.xml.ws.Endpoint;
+
+public class HelloWorldPublisher {
+
+    public static void main(String[]args) {
+        Endpoint.publish("http://localhost:9999/ws/hello", new HelloWorldImpl());
+    }
+}
