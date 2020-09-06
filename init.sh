0e84137e2fefefd6a12afee2383ab63d03f54f2c
Ляпин В <vvlyapin@dev.vtb>
2020-09-06 18:09:00 +0300

    Добавлены AuthorizationPolicy для всей цепочки взаимодействия

--- init.sh
+++ init.sh
@@ -119,6 +119,9 @@ docker rm -f $(docker container ls -q -a --filter name=${LOCAL_SRV_PROJECT})
 CONTAINER_CID=$(docker run -d --name ${LOCAL_SRV_PROJECT} -p ${CONTROLLER_PORT}:${CONTROLLER_PORT} -p $JMX_PORT:$JMX_PORT -e JAVA_OPTS="$MJO" ${LOCAL_NS}/${LOCAL_SRV_PROJECT}:${LOCAL_SRV_VERSION})
 echo "WebService container started. CONTAINER_CID="$CONTAINER_CID
 
+# Add authorization policy
+oc apply -f deny_authorizationPolicies.yaml
+echo "Deny all AuthorizationPolicy with restricted permissions were applied successfully!"
 
 # curl -v -HHost:mac.local.host mac.local.host:8080/ping
 # curl -v http://spring-boot-rest-server-svc.tutorial2.svc.cluster.local:8080/ping_chain
